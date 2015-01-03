00ff015c1380f1f6c21e7390f80a7aaad1332610
Antz <Antz@getMaNGOS.co.uk>
2015-01-03 03:07:50 +0000

    [Core] Revert code changed in error

--- src/game/ChatCommands/Level3.cpp
+++ src/game/ChatCommands/Level3.cpp
@@ -6590,7 +6590,7 @@ bool ChatHandler::HandleSendMessageCommand(char* args)
     WorldSession* rPlayerSession = rPlayer->GetSession();
 
     ///- Check that he is not logging out.
-    if (rPlayer->GetSession()->isLogingOut())
+    if (rPlayerSession->isLogingOut())
     {
         SendSysMessage(LANG_PLAYER_NOT_FOUND);
         SetSentErrorMessage(true);
@@ -6599,8 +6599,8 @@ bool ChatHandler::HandleSendMessageCommand(char* args)
 
     ///- Send the message
     // Use SendAreaTriggerMessage for fastest delivery.
-    rPlayer->GetSession()->SendAreaTriggerMessage("%s", args);
-    rPlayer->GetSession()->SendAreaTriggerMessage("|cffff0000[Message from administrator]:|r");
+    rPlayerSession->SendAreaTriggerMessage("%s", args);
+    rPlayerSession->SendAreaTriggerMessage("|cffff0000[Message from administrator]:|r");
 
     // Confirmation message
     std::string nameLink = GetNameLink(rPlayer);
